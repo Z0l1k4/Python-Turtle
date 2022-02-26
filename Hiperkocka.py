d091acc5fb7fdc64b5470d55d93e3bf4d3686970
Z0l1k4 <100485781+Z0l1k4@users.noreply.github.com>
2022-02-26 18:37:07 +0100

    Add files via upload

---
+++ Hiperkocka.py
@@ -0,0 +1,155 @@
+import turtle
+turtle.speed(5)
+
+
+turtle.penup()
+turtle.goto(-100, 0)
+turtle.pendown()
+
+turtle.pencolor("red")
+for i in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+
+
+turtle.pencolor("black")
+for i in range(1):
+  turtle.left(45)
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(0, 0)
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(-100, -100 )
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(0, -100)
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+  turtle.left(45)
+  turtle.forward(100)
+  turtle.left(90)
+  turtle.forward(100)
+  turtle.left(90)
+  turtle.forward(100)
+  turtle.left(90)
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(-100, 0)
+turtle.pendown()
+
+for i in range(1):
+  turtle.right(45)
+  turtle.forward(100)
+turtle.penup()  
+turtle.goto(0, 0)
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(-100, -100)
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(0, -100)
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(71, -29.8)
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(-29, 71)
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(-29, -29)
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(70, 71)
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(70, -70)
+turtle.pendown()
+
+
+turtle.pencolor("green")
+for i in range(1):
+  turtle.right(45)
+  turtle.forward(100)
+  turtle.right(90)
+  turtle.forward(100)
+  turtle.right(90)
+  turtle.forward(100)
+  turtle.right(90)
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(-29, -70)
+turtle.pendown()
+
+
+turtle.pencolor("black") 
+for i in range(1):
+  turtle.left(45)
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(70, -70)
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(42, -100)
+turtle.pendown()
+
+for i in range(1):
+  turtle.left(45)
+
+
+turtle.pencolor("blue")
+for i in range(4):
+  turtle.forward(100)
+  turtle.right(90)
+turtle.penup()
+turtle.goto(70, -170)
+turtle.pendown()
+
+
+turtle.pencolor("black")
+for i in range(1):
+  turtle.right(45)
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(-30, -170)
+turtle.pendown()
+
+for i in range(1):
+  turtle.forward(100)
+turtle.penup()
+turtle.goto(0, 150)
+turtle.pendown()
+
+turtle.write("Hiperkocka")
